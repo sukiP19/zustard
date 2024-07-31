0d9dbb067ea5fa6bb54ff04b06b2a66836842a3f
Suki Phan <suki@kiskolabs.com>
2024-07-31 11:45:25 +0300

    basic styling for components

---
+++ src/components/Column.jsx
@@ -0,0 +1,9 @@
+import "./Column.css";
+import Task from "./Task";
+
+export default function Column({state}) {
+    return <div className="column">
+        <p>{state}</p>
+        <Task title="Todo"/>
+    </div>
+}
---
+++ src/components/Task.jsx
@@ -0,0 +1,16 @@
+import classNames from "classnames";
+import "./Task.css";
+
+const STATUS = "PLANNED";
+
+export default function Task({title}) {
+    return (
+        <div className="task">
+            <div>{title}</div>
+            <div className="bottomWrapper">
+                <div></div>
+                <div className={classNames('status', STATUS)}>{STATUS}</div>
+            </div>
+        </div>
+    )
+}
